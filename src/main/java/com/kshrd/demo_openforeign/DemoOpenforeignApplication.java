34de9826a349d98ad0c098837727b340c3093945
pichet <good100vichet@gmail.com>
2025-08-31 09:47:46 +0700

    First commit

---
+++ src/main/java/com/kshrd/demo_openforeign/DemoOpenforeignApplication.java
@@ -0,0 +1,13 @@
+package com.kshrd.demo_openforeign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoOpenforeignApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoOpenforeignApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kshrd/demo_openforeign/DemoOpenforeignApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kshrd.demo_openforeign;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoOpenforeignApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
